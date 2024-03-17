c5209949930a067b6db0d9c8f12bd7013f9fb203
persd <dawid.g657@gmail.com>
2024-03-17 14:17:45 +0100

    feat: add header

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import "./index.css"
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#0091ea',
+      },
+      secondary: {
+        main: '#84ffff',
+      },
+    },
+});
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
